07ff5bf6d864402bef001db3a074a418ad61ca0f
EC2 Default User <ec2-user@ip-172-31-44-140.ap-northeast-1.compute.internal>
2025-12-23 10:23:51 +0000

    2ndデプロイ+テスト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_authentication
+
+  before_action :configure_permitted_parameters,
+   if: :devise_controller?
+    before_action :configure_authentication
  
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+
+    devise_parameter_sanitizer.permit(:account_update,keys:[:name])
+  end
  
   def configure_authentication
     if admin_controller?
