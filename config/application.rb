7ad26c31cb40ea696b593da075173475617dc234
EC2 Default User <ec2-user@ip-172-31-44-140.ap-northeast-1.compute.internal>
2026-02-01 12:46:33 +0000

    日本時間対応

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Muscle
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -15,8 +16,6 @@ module Muscle
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
